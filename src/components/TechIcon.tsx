afb381e7bccfaa5bc6235b0d86e50e7f7ad4845f
Sadrach Harmasantyo <harmasantyosadrach@gmail.com>
2024-10-07 00:56:09 +0700

    feat&fix: add about section also fix styles on card component and testimonials section

---
+++ src/components/TechIcon.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function TechIcon({ component }: { component: React.ElementType }) {
+  const Components = component;
+  return (
+    <>
+      <Components className="size-10 fill-[url(#tech-icon-gradient)]" />
+      <svg className="size-0 absolute">
+        <linearGradient id="tech-icon-gradient">
+          <stop offset="0%" stopColor="rgba(110 231 183)" />
+          <stop offset="100%" stopColor="rgba(56 189 248)" />
+        </linearGradient>
+      </svg>
+    </>
+  );
+}
